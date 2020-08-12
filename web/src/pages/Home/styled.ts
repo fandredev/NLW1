050a72fce3910928bc3423fe0c68b4f3fcd1cd5a
profissionalf.andre@gmail.com <profissionalf.andre@gmail.com>
2020-08-12 14:56:48 -0300

    feat: add in pkg.json styled-components and run seed and mgrations]

---
+++ web/src/pages/Home/styled.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+
+export const PageHome = styled.div`
+  height: 100vh;
+
+  background: url('../../assets/home-background.svg') no-repeat 700px bottom
+
+`
+export const Content = styled.div`
+    width: 100%;
+  height: 100%;
+  max-width: 1100px;
+  margin: 0 auto;
+  padding: 0 30px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`
